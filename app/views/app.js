408abce26304e15fd44b9af8a850b7ddd9b44e05
wahack <liaoyux@gmail.com>
2015-10-09 17:35:18 +0800

    custom Navigator

--- app/views/app.js
+++ app/views/app.js
@@ -1,16 +1,32 @@
 'user strict';
 var React = require('react-native');
-var Router = require('react-native-router');
+// var Router = require('react-native-router');
+const {Navigator} = React;
 
+var Index = require('./index');
 var Login = require('./login');
-var firstRoute = {
-  name: '登录',
-  component: Login
-};
+var Signup = require('./signup');
+//
+// var firstRoute = {
+//   name: '我的主页',
+//   component: Index
+// };
 module.exports = React.createClass({
-  render: function () {
-    return (<Router
-      firstRoute = {firstRoute}
-    />);
+  _renderScene: function (route, navigator) {
+    switch (route.index) {
+      case 1:
+        return <Index navigator={navigator} />
+      case 2:
+        return <Login navigator={navigator} />
+      case 3:
+        return <Signup navigator={navigator} />
+      default:
+        return <Index navigator={navigator} />
+    }
+  },
+  render() {
+    return (
+      <Navigator initialRoute={{name:'login', index:2}} renderScene={this._renderScene} />
+    )
   }
 });
