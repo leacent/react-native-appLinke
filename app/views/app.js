0c1c8ca45c6b8e912efb110332f2f33441decdbc
wahack <liaoyux@gmail.com>
2015-10-09 15:01:52 +0800

    delete comments

--- app/views/app.js
+++ app/views/app.js
@@ -2,18 +2,6 @@
 var React = require('react-native');
 var Router = require('react-native-router');
 
-// var stores = require('../store');
-// var actions = require('../actions');
-// var Fluxxor = require('fluxxor');
-//
-// var flux = new Fluxxor.Flux(stores, actions);
-
-// flux.on("dispatch", function (type, payload) {
-//   if (console && console.log) {
-//     console.log('%c[Dispatch] %c', 'color: #006600;', 'color: #000;', type, payload);
-//   }
-// });
-
 var Login = require('./login');
 var firstRoute = {
   name: '登录',
